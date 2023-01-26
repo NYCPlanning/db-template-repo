dd399f011981a71fad9292e7509f527ea8940544
mbh329 <mbrueckner@planning.nyc.gov>
2023-01-26 16:42:35 +0000

    add config with set env func

---
+++ bash/config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Set Environmental Variables 
+function set_env {
+  for envfile in $@
+  do
+    if [ -f $envfile ]
+      then
+        export $(cat $envfile | sed 's/#.*//g' | xargs)
+      fi
+  done
+}
