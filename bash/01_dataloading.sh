559801362755ea78979adc46f716df569e24c99d
mbh329 <mbrueckner@planning.nyc.gov>
2023-01-26 14:19:28 -0500

    add postgres install to docker, create initial dataloading, add config.sh functions

--- bash/01_dataloading.sh
+++ bash/01_dataloading.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 source bash/config.sh
 
+# Run postgres with less output and create a table of versions by dataset
+psql $BUILD_ENGINE --quiet --command "
+  DROP TABLE IF EXISTS versions;
+  CREATE TABLE versions ( 
+    datasource text, 
+    version text 
+  );
+"
+
+#Import Data
+import dcp_mappluto_wi
+wait
+
+rm -rf .library
