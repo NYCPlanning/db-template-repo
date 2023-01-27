414d7d657b8544795ac3bb2b47fafd817e2793f2
Damon McCullough <dmccullough@planning.nyc.gov>
2023-01-27 14:56:11 -0500

    create data loading step (#6)
    * add bash folder and dataloading sh
    
    * add config with set env func
    
    * create/copy function from other repos to set config file
    
    * add postgres install to docker, create initial dataloading, add config.sh functions
    
    * add gis features to dataloading
    
    * ignore data cache
    
    * configure minio s3 connection
    
    * add .env example with required envar names
    
    * tweak devcontainer text
    
    * delete placeholder bash script
    
    * move bash functions to new utils file
    
    * util comments
    
    * try to add a gha to Actions tab before merging
    
    * branches from dev should trigger tests
    
    * trigger tests on branches from dev
    
    * try running dataloading (should fail)
    
    * oops
    
    * ...
    
    * ...
    
    * use working-directory
    
    * oops
    
    * specify file
    
    * better job name
    
    * try to exit when any command fails
    
    * move exit and debug traps to config.sh
    
    * remove some debug lines
    
    * call primary container runner instead of app
    
    * remove poetry until it's needed
    
    * convert build tests to placeholders
    
    * don't delete data cache
    
    ---------
    
    Co-authored-by: mbh329 <mbrueckner@planning.nyc.gov>

---
+++ bash/01_dataloading.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+source bash/config.sh
+
+# Run postgres with less output and create a table of versions by dataset
+psql $BUILD_ENGINE --quiet --command "
+  DROP TABLE IF EXISTS versions;
+  CREATE TABLE versions ( 
+    datasource text, 
+    version text 
+  );
+"
+
+#Import Data
+import dcp_mappluto_wi &
+
+# Import GIS Features
+import dcp_commercialoverlay &
+import dcp_limitedheight &
+import dcp_specialpurpose &
+import dcp_specialpurposesubdistricts &
+import dcp_zoningmapamendments &
+import dcp_zoningdistricts &
+import dcp_zoningmapindex &
+wait
+
+# # Delete data cache
+# rm -rf .library
