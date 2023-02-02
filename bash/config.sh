d5329b5cd4e1de79212bec49ce083672cffacf3f
Damon McCullough <dmccullough@planning.nyc.gov>
2023-02-02 12:17:27 -0500

    comments

--- bash/config.sh
+++ bash/config.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# A script used by build scripts to import utility functions, set environment variables, and
+
 # Exit when any command fails
 set -e
-# keep track of the last executed command
+# Keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
+# Echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 source bash/utils.sh
