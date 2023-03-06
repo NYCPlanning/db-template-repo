cb8d287321d4ca5f2aacdb78802704d24ffc656b
Damon McCullough <dmccullough@planning.nyc.gov>
2023-03-06 12:41:17 -0500

    debug

--- bash/setup_devcontainer.sh
+++ bash/setup_devcontainer.sh
@@ -2,7 +2,8 @@
 #
 # Sets up enviroment for dev in local devcontainer.
 set -e
-
+echo "Setting up devcontainer ..."
+echo "CI envar is $CI"
 # Skip when called in a github action workflow
 if [ !$CI ]; then
     # Add id_rsa so that we can push to github from the dev container
