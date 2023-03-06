e34b14ae79fe89c99dfa3be22e821d7a2abf2d79
Damon McCullough <dmccullough@planning.nyc.gov>
2023-03-06 13:42:08 -0500

    use strings for boolean

--- bash/setup_devcontainer.sh
+++ bash/setup_devcontainer.sh
@@ -4,8 +4,9 @@
 set -e
 echo "Setting up devcontainer ..."
 echo "CI envar is $CI"
+
 # Skip when called in a github action workflow
-if [ !$CI ]; then
+if [ !$CI == "true"]; then
     # Add id_rsa so that we can push to github from the dev container
     ssh-add $HOME/.ssh/id_rsa
 fi
