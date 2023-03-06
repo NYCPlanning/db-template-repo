f2fcacd1a43511eed8a57b42a533ef1c2babe150
Damon McCullough <dmccullough@planning.nyc.gov>
2023-03-06 11:01:27 -0500

    better staging build step

--- bash/02_staging.sh
+++ bash/02_staging.sh
@@ -4,6 +4,6 @@ source bash/build_config.sh
 echo "Dataset Version $VERSION : 02 Staging"
 
 echo "Preprocess source data ..."
-run_sql_file sql/preprocessing.sql
+run_sql_file sql/preprocess.sql
 
 echo "Done!"
