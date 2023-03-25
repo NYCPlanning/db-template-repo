6fbf9af9e813a794c306dcfc4e67fcb5631acb4c
Damon McCullough <damonmcc1391@gmail.com>
2023-03-25 14:39:39 -0400

    compile requirements

--- bash/dev_compile_python_packages.sh
+++ bash/dev_compile_python_packages.sh
@@ -4,11 +4,15 @@
 set -e
 
 # Update and install packages used to compile requirements
+echo -e "🛠 upgrading python package management tools"
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade pip-tools wheel
 
+echo -e "🛠 deleting requirements.txt"
 # Delete exisitng requirements file to ensure full dependency resolution
 rm --force requirements.txt
 
 # Compile requirements
+echo -e "🛠 compiling requirements.txt"
 CUSTOM_COMPILE_COMMAND="./bash/dev_python_packages.sh" python3 -m piptools compile --output-file=requirements.txt requirements.in
+echo -e "✅ done compiling requirements.txt"
