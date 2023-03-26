77755681e495209cee189ca66f24d6bce1e2ac8b
Damon McCullough <damonmcc1391@gmail.com>
2023-03-26 17:33:40 +0000

    add and use map utils

--- python/utils.py
+++ python/utils.py
@@ -1,24 +1,34 @@
 import os
 import pandas as pd
 import geopandas as gpd
+import contextily as cx
+from matplotlib.axes import Axes
+from folium.folium import Map
+from xyzservices import TileProvider
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import Session
 
 SQL_FILE_DIRECTORY = "sql"
+NYC_PROJECTION = "EPSG:2263"
+DEFAULT_BASEMAP = cx.providers.Stamen.Terrain
+DEFAULT_MAP_CONFIG = {
+    "figsize": (10, 10),
+    "alpha": 0.5,
+    "edgecolor": "g",
+    "linewidths": 3,
+}
 
 
 def load_csv_file(directory: str, filename: str) -> pd.DataFrame:
-    data = pd.read_csv(f"{directory}/{filename}")
-    data.columns = data.columns.str.lower()
-
-    return data
+    return pd.read_csv(f"{directory}/{filename}")
 
 
 def load_shapefile(directory: str, filename: str) -> gpd.GeoDataFrame:
-    data = gpd.read_file(f"{directory}/{filename}")
-    data.columns = data.columns.str.lower()
+    return gpd.read_file(f"{directory}/{filename}")
 
-    return data
+
+def load_geodata_url(url: str) -> gpd.GeoDataFrame:
+    return gpd.read_file(url)
 
 
 def execute_sql_command(command: str) -> None:
@@ -62,3 +72,47 @@ def get_source_data_versions() -> pd.DataFrame:
         select_records = pd.read_sql_table("source_versions", connection)
 
     return select_records
+
+
+def pad_map_bounds(
+    data: gpd.GeoDataFrame,
+    axes: Axes,
+    x_scale: float,
+    y_scale: float,
+) -> Axes:
+    lon_min, lat_min, lon_max, lat_max = data.total_bounds
+    lon_pad, lat_pad = y_scale * (lon_max - lon_min), x_scale * (lat_max - lat_min)
+
+    axes.axis("scaled")
+    axes.axis(
+        [lon_min - lon_pad, lon_max + lon_pad, lat_min - lat_pad, lat_max + lat_pad]
+    )
+    return axes
+
+
+def map_simple(
+    data: gpd.GeoDataFrame,
+    projection: str = NYC_PROJECTION,
+    basemap: str | TileProvider = DEFAULT_BASEMAP,
+    **kwargs,
+) -> Axes:
+    axes = data.plot(
+        **kwargs,
+    )
+    cx.add_basemap(
+        axes,
+        crs=projection,
+        source=basemap,
+    )
+    axes.set_axis_off()
+
+    return axes
+
+
+def map_folium(data: gpd.GeoDataFrame, **kwargs) -> Map:
+    # GeoDataFrame.explore fails if any columns have a dtype of object
+    for column in data.columns:
+        if isinstance(column, object) and column != "geometry":
+            data[column] = data[column].astype(str)
+
+    return data.explore(**kwargs)
