5478eba5e660f5928629847be3cb18c8a6ff560e
Damon McCullough <damonmcc1391@gmail.com>
2023-04-28 17:45:08 +0000

    little more test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,12 +11,14 @@ def test_load_data_file_csv():
     data = load_data_file(filepath=f"{TEST_DATA_PATH}/minimal_data.csv")
     assert isinstance(data, pd.DataFrame)
     assert len(data) == 3
+    assert data["column_b"][2] == "b_2"
 
 
 def test_load_data_file_json():
     data = load_data_file(filepath=f"{TEST_DATA_PATH}/minimal_data.json")
     assert isinstance(data, pd.DataFrame)
     assert len(data) == 2
+    assert data.iloc[1]["key_int"] == 300
 
 def test_load_data_file_other():
     with pytest.raises(NotImplementedError):
