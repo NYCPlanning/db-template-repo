e63157977ce5d18ef1a031e9a47173c2389a3558
Damon McCullough <damonmcc1391@gmail.com>
2023-04-28 01:34:39 +0000

    expand and rename test examples

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -2,18 +2,62 @@
 import pytest
 
 # ensure imports or project modules work
-from python.utils import execute_sql_file
+from python.utils import load_data_file
 
 
 def test_always_passes():
     assert True
 
 
-@pytest.mark.xfail(reason="expected to always fail")
-def test_expect_always_fails():
+@pytest.mark.skip(reason="skipping failing test")
+def test_skip_failing_test():
     assert False
 
 
-@pytest.mark.skip(reason="skipping always fails")
-def test_skip_always_fails():
+@pytest.mark.xfail(reason="test expected fail")
+def test_expect_failing_test():
     assert False
+
+
+@pytest.mark.parametrize("test_input", [0, 1])
+def test_parametrize_input(test_input):
+    assert test_input >= 0
+
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        (0, 0),
+        (1, 2),
+    ],
+)
+def test_parametrize_input_expected(test_input, expected):
+    assert test_input * 2 == expected
+
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        ("3 + 5", 8),
+        ("2 + 4", 6),
+        pytest.param(
+            "6 * 9",
+            42,
+            marks=pytest.mark.xfail,
+            id="test_input param expected to fail",
+        ),
+    ],
+)
+def test_parametrize_complex(test_input, expected):
+    assert eval(test_input) == expected
+
+
+@pytest.mark.parametrize("x", [0, 1])
+@pytest.mark.parametrize("y", [2, 3])
+def test_parametrize_all_combinations(x, y):
+    pass
+
+
+@pytest.mark.slow_test()
+def test_slow_test():
+    assert True
